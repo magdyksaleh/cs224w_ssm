7b8c1759c10f450a3c6878faecc8fa5630fb153e
magdy <magdyksaleh@gmail.com>
2018-11-10 23:50:23 -0800

    refactor

--- src/data_cleaning/Propagation_Algorithm.m
+++ src/data_cleaning/Propagation_Algorithm.m
@@ -30,49 +30,4 @@ numParam = numel(data);
 %correcting flows for improved flow conservation 
 [node_filtered,seg_filtered,seg_filtered_Index] = correctFlows(node_filtered,seg_filtered,seg_filtered_Index,data);
 disp('Segments filtered, zero flow regions identified')
-
-%Identifing inlets to the network
-[Inlet_Index, Inlet] = findInlets(node_filtered,seg_filtered,seg_filtered_Index);
-%Identifing outlets to the network
-[Outlet_Index, Outlet] = findOutlets(node_filtered,seg_filtered,seg_filtered_Index);
-
-%Find the flow rate factors
-[scale, scale_Index] = findFlowRatios(node_filtered,seg_filtered_Index,seg_filtered);
-
-%MP_Constant is the membrance permeability constant across the network
-MP_Constant = 1e-5;
-seg_filtered = getPermeability(MP_Constant, seg_filtered);
-
-%Summing the total Flux into the network as the sum of the individual inlet
-%fluxes
-total_Inflowing_Flux = sum(cat(1,Inlet.Flux));
-
-%defining the ratio of fluxes for each inlet as a percentage
-for i = 1:numel(Inlet)
-    Inlet(i).Scale = Inlet(i).Flux/total_Inflowing_Flux;
-end
-
-disp('Inlet Adjustment Complete');
-
-%merging the segment and node information into one structure to eliminate
-
-for i = 1:numel(node_filtered)
-    for j = 1:numel(node_filtered(i).connectionOut)/3
-        curSeg = node_filtered(i).connection(j,:);
-        locSeg = findRowInIndexV(curSeg,seg_filtered_Index,3);
-        locScale = findRowInIndexV(curSeg,scale_Index,3);
-        locInlet = findRowInIndexV(curSeg,Inlet_Index,3);
-        node_filtered(i).Scale(j,1) = scale(locScale).Val;
-        node_filtered(i).Flux(j,1) = seg_filtered(locSeg).Flux;
-        node_filtered(i).Delay(j,1) = seg_filtered(locSeg).Delay;
-        node_filtered(i).Volume(j,1) = seg_filtered(locSeg).Volume;
-        node_filtered(i).locSeg(j,1) = locSeg;
-        if (~isempty(locInlet)) 
-            seg_filtered(locSeg).InletScale(1,1) = Inlet(locInlet).Scale(1,1);
-        end
-        if isfield(seg_filtered,'pressureDrop')
-            node_filtered(i).pressureDrop(j,1) = seg_filtered(locSeg).pressureDrop;
-        end
-    end
-end
 disp('Preprocessing Complete');
--- src/data_cleaning/filter.py
+++ src/data_cleaning/filter.py
@@ -6,13 +6,9 @@ LS174T_data = sio.loadmat('SW1222_data.mat')
 edges = []
 for elem in LS174T_data['seg_filtered'][0]:
     edgeRef = elem[0][0]
-    # print edgeRef[:-1]
-    edges.append(tuple(edgeRef[:-1]))
-    if edgeRef[2] != 0:
-        for i in range(edgeRef[2]):
-            edges.append(tuple(edgeRef[:-1]))
+    [edges.append(tuple(edgeRef[:-1])) for i in range(edgeRef[2]+1)]
 
-outFile = open("../../data/SW1222_clean_EdgesList.txt", "w")
+outFile = open("../../data/SW1222__clean_EdgesList.txt", "w")
 for edge in edges:
     outFile.write(str(edge[0]) + "\t" + str(edge[1])+ "\n")
 outFile.close()
